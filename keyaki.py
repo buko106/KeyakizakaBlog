235309d62e4b21ae4447eeaff2c7dc084c815e2e
buko106 <buko1062000@yahoo.co.jp>
2017-10-31 12:52:35 +0900

    get latest diary info

--- keyaki.py
+++ keyaki.py
@@ -5,14 +5,39 @@ from bs4 import BeautifulSoup
 
 
 class Keyaki:
+    """
+    Method whose name begins with "get" actualy creates HTTP request,
+    and returns response. Method whose name begins with "parse" converts
+    response to a dict including infomation obtaind from response.
+    """
     def __init__(self):
-        self.ENTRYPOINT_PREFIX = "http://www.keyakizaka46.com/s/k46o/"
-        self.ENTRYPOINT_ARTIST = self.ENTRYPOINT_PREFIX + "artist/"
-        self.ENTRYPOINT_DIARY_MEMBER = self.ENTRYPOINT_PREFIX + "diary/member/"
-        self.ENTRYPOINT_DIARY_DETAIL = self.ENTRYPOINT_PREFIX + "diary/detail/"
+        self.ENTRYPOINT_DOMAIN = "http://www.keyakizaka46.com"
+        self.ENTRYPOINT_PREFIX = self.ENTRYPOINT_DOMAIN + "/s/k46o"
+        self.ENTRYPOINT_ARTIST = self.ENTRYPOINT_PREFIX + "/artist"
+        self.ENTRYPOINT_DIARY_MEMBER = self.ENTRYPOINT_PREFIX + "/diary/member"
+        self.ENTRYPOINT_DIARY_DETAIL = self.ENTRYPOINT_PREFIX + "/diary/detail"
+        self.MAXIMUM_CT = 42
 
-    def text_to_soup(self, text):
+    @staticmethod
+    def text_to_soup(text):
         return BeautifulSoup(text, "html.parser")
+
+    @staticmethod
+    def convert_ct(ct):
+        try:
+            ct = "%02d" % int(ct)
+            return ct
+        except ValueError:
+            print("Invalid value of ct")
+            raise
+
+    def _convert_href_to_url_and_extract_id(self, input_dict):
+        import copy
+        result_dict = copy.deepcopy(input_dict)
+
+        result_dict["id"] = result_dict["href"].split("?")[0].split("/")[-1]
+        result_dict["url"] = self.ENTRYPOINT_DOMAIN + result_dict["href"]
+        return result_dict
 
     def get(self, url, params=None):
         response = requests.get(url, params=params)
@@ -26,28 +51,73 @@ class Keyaki:
             print("Invalid id in get_diary_detail")
             raise
 
-        url = self.ENTRYPOINT_DIARY_DETAIL + id
+        url = self.ENTRYPOINT_DIARY_DETAIL + "/" + id
         params = {
             "ima": "0000",
         }
 
         response = self.get(url, params=params)
         return response
+
+    def _get_global_latest_diary(self):
+        url = self.ENTRYPOINT_DIARY_MEMBER
+        return self.get(url)
+
+    def _get_latest_diary_by_ct(self, ct):
+        ct = self.convert_ct(ct)
+        url = self.ENTRYPOINT_DIARY_MEMBER + "/" + "list"
+        params = {
+            "ima": "0000",
+            "ct": ct,
+        }
+        return self.get(url, params=params)
+
+    def _parse_diary_member_list(self, response):
+        soup = self.text_to_soup(response.text)
+        box_ttl = soup.find(attrs={"class": "box-ttl"})
+        href = box_ttl.h3.a["href"]
+        title = box_ttl.h3.a.string
+        return {
+            "href": href,
+            "title": title,
+        }
+
+    def _parse_global_latest_diary(self, response):
+        soup = self.text_to_soup(response.text)
+        slider = soup.find("div", attrs={"class": "slider"})
+        return {
+            "href": slider.ul.li.a["href"],
+            "title": slider.ul.li.a.string,
+        }
+
+    def latest_diary(self, ct=None):
+        if ct is None:
+            result = self._parse_global_latest_diary(
+                self._get_global_latest_diary()
+            )
+            return self._convert_href_to_url_and_extract_id(result)
+        else:
+            result = self._parse_diary_member_list(
+                self._get_latest_diary_by_ct(ct)
+            )
+            return self._convert_href_to_url_and_extract_id(result)
 
     def get_artist(self, ct):
-        try:
-            ct = "%02d" % int(ct)
-        except ValueError:
-            print("Invalid ct in artist")
-            raise
-
-        url = self.ENTRYPOINT_ARTIST + ct
+        ct = self.convert_ct(ct)
+        url = self.ENTRYPOINT_ARTIST + "/" + ct
         params = {
             "ima": "0000",
         }
 
         response = self.get(url, params=params)
         return response
+
+    def artist(self, ct=None):
+        if ct is None:
+            return  # TODO
+        else:
+            ct = self.convert_ct(ct)
+            return self.parse_artist(self.get_artist(ct))
 
     def parse_diary_detail(self, response):
         soup = self.text_to_soup(response.text)
@@ -86,6 +156,7 @@ class Keyaki:
         furigana = box_profile_text.find(
             attrs={"class": "furigana"}
         ).string.strip()
+        # NOTE: replace white space(\u3000) with ascii white space
         en = box_profile_text.find(
             attrs={"class": "en"}
         ).string.strip().replace("\u3000", " ")
